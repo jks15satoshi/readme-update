15bb716422fb1bf46a89d049d6d550eb3fbc1476
Satoshi Jek <jks15satoshi@gmail.com>
2021-09-01 17:57:49 +0800

    fix: Bug on updating contents

--- readme_update/parse.py
+++ readme_update/parse.py
@@ -1,16 +1,17 @@
 import re
 from pathlib import Path
 
-regex: re.Pattern = re.compile(
-    r'(?P<start><!-- (?P<tag>\w*) (starts|begins) -->).*?'
-    r'(?P<end><!-- (?P=tag) ends -->)',
-    flags=re.IGNORECASE | re.DOTALL)
-
 
 def parse_readme() -> None:
-    """Parse the README file and update the contents.
     """
-    with (Path(__file__).parents[1] / 'test.md').open('r+') as file:
+    Parse the README file and update the contents.
+    """
+    regex: re.Pattern = re.compile(
+        r'(?P<start><!-- (?P<tag>\w*) (starts|begins) -->).*?'
+        r'(?P<end><!-- (?P=tag) ends -->)',
+        flags=re.IGNORECASE | re.DOTALL)
+
+    with (Path(__file__).parents[1] / 'README.md').open('r+') as file:
         contents = file.read()
         file.seek(0)
         result = regex.sub(_parse_tag, contents)
@@ -19,7 +20,8 @@ def parse_readme() -> None:
 
 # TODO: logics of updating contents.
 def _parse_tag(match: re.Match) -> str:
-    """Parse tags from the comment blocks and update contents according to the
+    """
+    Parse tags from the comment blocks and update contents according to the
     tags.
 
     Args:
@@ -29,4 +31,4 @@ def _parse_tag(match: re.Match) -> str:
     - `str`: Contents to be updated.
     """
     result = ''
-    return r'\g<start>\n{}\n\g<end>'.format(result)
+    return ''.join([match.group('start'), f'\n{result}\n', match.group('end')])
