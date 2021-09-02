e504c0ef24fb8d50d3d785d875a50a940c286054
Satoshi Jek <jks15satoshi@gmail.com>
2021-09-02 18:37:24 +0800

    improv: Update implementation on Markdown parsing.

--- readme_update/parse.py
+++ readme_update/parse.py
@@ -1,17 +1,23 @@
 import re
 from pathlib import Path
 
+root = Path(__file__).parents[1].resolve()
 
-def parse_readme() -> None:
+
+def parse_readme(readme: Path = root / 'README.md') -> None:
     """
     Parse the README file and update the contents.
+
+    Args:
+    - `readme (pathlib.Path)`: Path to the README file.
     """
     regex: re.Pattern = re.compile(
-        r'(?P<start><!-- (?P<tag>\w*) (starts|begins) -->).*?'
+        r'(?P<start><!-- (?P<tag>\w{,20}) (starts|begins) -->)'
+        r'(?P<contents>.*?)'
         r'(?P<end><!-- (?P=tag) ends -->)',
         flags=re.IGNORECASE | re.DOTALL)
 
-    with (Path(__file__).parents[1] / 'README.md').open('r+') as file:
+    with readme.open('r+') as file:
         contents = file.read()
         file.seek(0)
         result = regex.sub(_parse_tag, contents)
@@ -30,5 +36,10 @@ def _parse_tag(match: re.Match) -> str:
     Returns:
     - `str`: Contents to be updated.
     """
+    delimiter = '\n' if ((contents := match.group('contents')).startswith('\n')
+                         or contents.endswith('\n')) else ''
     result = ''
-    return ''.join([match.group('start'), f'\n{result}\n', match.group('end')])
+    return ''.join([
+        match.group('start'), delimiter, result, delimiter,
+        match.group('end')
+    ])
